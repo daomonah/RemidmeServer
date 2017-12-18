81deb974c346b10895548badb600e02c071426f3
Yuriy Pochynok <yuriy.pochynok@gmail.com>
2017-12-18 18:40:58 +0200

    Started DB configuration

--- src/main/java/com/daomonah/remindme/server/ApplicationInitializer.java
+++ src/main/java/com/daomonah/remindme/server/ApplicationInitializer.java
@@ -5,11 +5,9 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
-public class ApplicationInitializer implements WebApplicationInitializer{
+public class ApplicationInitializer implements WebApplicationInitializer {
 
     private static final String DISPATCHER = "dispatcher";
 
